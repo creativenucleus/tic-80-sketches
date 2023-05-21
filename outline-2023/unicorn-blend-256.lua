2d8bd576e93fe48decb7c2a91229f30e531ec224
James R <creativenucleus@users.noreply.github.com>
2023-05-21 16:28:14 +0200

    Update unicorn-blend-256.lua

--- outline-2023/unicorn-blend-256.lua
+++ outline-2023/unicorn-blend-256.lua
@@ -1,4 +1,4 @@
--- name: Unicorn Blend 256
+-- title: Unicorn Blend 256
 -- author: jtruk
 -- date: 2023-05-18
 -- release: Outline 2023
@@ -18,8 +18,8 @@ function TIC()
   j,k,l=D(0),D(.3),D(.6)
   tri(
    j.x,j.y,k.x,k.y,l.x,l.y,
-			8+C(i*C(T*.003))*7
-		)
+   8+C(i*C(T*.003))*7
+  )
  end
  T=T+1
 end
@@ -29,31 +29,6 @@ function D(a)
  x,y=C(a)*L,C(a-11)*L
  return {x=120+x+y*C(T*.02),y=68+y}
 end
-
--- <TILES>
--- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
--- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
--- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
--- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
--- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
--- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
--- </TILES>
-
--- <WAVES>
--- 000:00000000ffffffff00000000ffffffff
--- 001:0123456789abcdeffedcba9876543210
--- 002:0123456789abcdef0123456789abcdef
--- </WAVES>
-
--- <SFX>
--- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
--- </SFX>
-
--- <TRACKS>
--- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
--- </TRACKS>
 
 -- <SCREEN>
 -- 000:333333333333bbbbbbbbbbbbbbbbeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeecccccccccccceeeeeeeeeeedddddddddddddddddddddddddd888888888881111111111111111111111111111addddddddddddddddeeeeeeeeeeeeeee33333333333333333333333333333333333dddd9999999999999999999999
@@ -197,4 +172,3 @@ end
 -- <PALETTE>
 -- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
 -- </PALETTE>
-
