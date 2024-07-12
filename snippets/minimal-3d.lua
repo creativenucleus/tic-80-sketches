66e901c45001cee03b8849a0e437d326072eefe3
creativenucleus <github.com@creativenucleus.com>
2024-07-12 18:49:40 +0100

    Updated minimal-3d example

--- snippets/minimal-3d.lua
+++ snippets/minimal-3d.lua
@@ -9,13 +9,43 @@
 --  a z-buffer
 
 -- make some local aliases - for clean code and speed!
-local SIN,COS=math.sin,math.cos
+local SIN,COS,PI=math.sin,math.cos,math.pi
+local MIN,MAX=math.min,math.max
+local TAU=2*PI
+local CAMX,CAMY,CAMZ=0,0,0
 
 function TIC()
 	T=time()*.03
 
 	cls()
 	
+	-- +ve is right,down,into-screen	
+	CAMZ=-9
+	CAMY=-1.5
+ 
+ drawFloor()
+ drawSpheres({x=0,y=-2,z=0},3,T*.05)
+end
+
+function drawSpheres(centre,radius,t)
+ local nSpheres=5
+ for i=1,nSpheres do
+  local aOfTau=(i/nSpheres)*TAU
+  local a=aOfTau+t
+  local p={
+  	x=COS(a)*radius,
+   y=SIN(aOfTau+t*2),
+   z=SIN(a)*radius,
+  }
+
+		p=trans(p,centre)
+		p=proj(p)
+		local size=MIN(MAX(20*p.z,.1),40)
+		circ(p.x,p.y,size,12)
+ end
+end
+
+function drawFloor()	
 	-- mr? = model rotation in world
 	local mrX=0 --T*.02
 	local mrY=0 --T*.025
@@ -23,7 +53,7 @@ function TIC()
 
 	-- mt? = model transform in world
 	local mtX=0 --SIN(T*.1)
-	local mtY=3 --SIN(T*.07)
+	local mtY=0 --SIN(T*.07)
 	local mtZ=0 --SIN(T*.08)
 
 	-- lt? = local transform of tile within model
@@ -121,13 +151,42 @@ end
 -- 120,68 is the pixel centre of the screen
 -- All the rest are magic numbers
 function proj(p)
-	local camZ=6
-	local scaleZ=3
-	local pixScale=100
-	local zD=camZ-p.z/scaleZ
+	local projScale=2
+	local zD=projScale/(p.z-CAMZ)
+	local pixScale=80
 	return {
-		x=120+p.x/zD*pixScale,
-		y=68+p.y/zD*pixScale,
+		x=120+(p.x-CAMX)*zD*pixScale,
+		y=68+(p.y-CAMY)*zD*pixScale,
 		z=zD,
 	}
 end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
